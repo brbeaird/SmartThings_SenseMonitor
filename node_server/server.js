20646f5d76962b376a7fc103f4a4355e86f8e152
Brian Beaird <brbeaird@gmail.com>
2018-08-04 15:54:38 -0500

    Total device, naming, and error improvements Temporarily override unofficial-sense library until close emitter is added Add specific threshold for Total device Improved name handling for "guess" devices Improved close/error handling

--- node_server/server.js
+++ node_server/server.js
@@ -7,18 +7,19 @@ var smartThingsHubIP = "ReplaceWithYourHubIP"
 //Optional Settings
 /*This process opens up a websocket connection to Sense; we see realtime data from Sense every couple seconds.
 While parsing that data, we watch for significant changes so we can push to SmartThings only when relevant to avoid spamming the hub.
-At baseline, we send an update at least once every 5 minutes (maximumsecondsBetweenPush).
+At baseline, we send an update at least once every 3 minutes (maximumsecondsBetweenPush).
 We do NOT send updates less than 5 seconds apart (minimumSecondsBetweenPush).
 We send an update when at least one device has turned on or off.
 We send an update when power usage on a device has changed by at least 100 watts (usageThreshold).
 */
-var usageThreshold = 100;    //Change in usage that determines when a special push to ST is made
-var maximumsecondsBetweenPush = 300; //Maximum number of seconds between data pushes to ST
-var minimumSecondsBetweenPush = 5; //Minimum number of seconds between data pushes to ST
+var usageThreshold = 200;    //Change in usage that determines when a special push to ST is made
+var totalUsageThreshold = 100;    //Change in usage that determines when the "total" device is marked as recently changed
+var maximumsecondsBetweenPush = 180; //Maximum number of seconds between data pushes to ST
+var minimumSecondsBetweenPush = 30; //Minimum number of seconds between data pushes to ST
 var autoReconnect = true;
 
 //Libraries
-const sense = require('unofficial-sense');
+const sense = require('./unofficial-sense');
 var rp = require("request-promise");
 
 //Global variables to keep track of various things
@@ -26,6 +27,7 @@ global.deviceList = {};
 global.lastPush = new Date();
 lastPush.setDate(lastPush.getDate()-1);
 global.reconnectPending = false;
+global.prevTotalUsage = 0;
 
 getData();
 
@@ -39,13 +41,18 @@ function getData(){
         console.log("Successfully connected to Sense! Data incoming!")
         reconnectPending = false;
         mySense.getDevices().then(devices => {
-            for (let dev of devices){
+            for (let dev of devices){                
+                let devName = dev.name;
+                if (dev.tags.NameUserGuess == "true"){ 
+                    devName = devName + " (?)" 
+                }
                 deviceList[dev.id] = {
                     id: dev.id,
-                    name: dev.name,
+                    name: devName,
                     state: "unknown",
                     usage: -1,
-                    refreshed: 0
+                    currentlyOn: false,
+                    recentlyChanged: true
                 }
             }        
         })
@@ -54,10 +61,8 @@ function getData(){
         mySense.events.on('data', processData);        
         
         //Handle closures and errors        
-        mySense.events.on('close', handleWSClose, {data: this, msg: 'Connection was closed.'});
-        mySense.events.on('error', handleWSClose, {data: this, msg: 'An error occurred' + this});
-
-        //mySense.events.on('close', (data) => {
+        mySense.events.on('close', (data) => {handleWSClose({eventData: data, msg: 'Connection closed.'});});
+        mySense.events.on('error', (data) => {handleWSClose({eventData: data, msg: 'An error occurred. ' + data});});
 
         function handleWSClose(data){                        
             console.log(data.msg);
@@ -81,10 +86,12 @@ function getData(){
                 if (data.payload != undefined){
                     if (data.payload.devices != undefined){                        
                         let updateNow = false;
+                        let usageUpdate = false;
 
                         //Mark off saved list so we can detect which have been seen lately
                         Object.keys(deviceList).forEach(function (key){                            
-                            deviceList[key].refreshed = 0;
+                            deviceList[key].currentlyOn = false;                            
+                            if (deviceList[key].usage != -1){deviceList[key].recentlyChanged = false;}  //Reset recentChange flag unless it's initial load                            
                         })
 
                         //Loop over currently active devices and refresh the saved list
@@ -93,20 +100,22 @@ function getData(){
                             let prevState = deviceList[dev.id].state;
                             let prevUsage = deviceList[dev.id].usage;
                             let currentUsage = dev.w;
-                            let usageDelta = currentUsage - prevUsage;                                                       
+                            let usageDelta = currentUsage - prevUsage;
                             
-                            if (prevState != "on" && prevState != "unknown" && deviceList[dev.id].name != "Other"){
-                                console.log(dev.name + " turned on!");
+                            if (prevState != "on" && prevState != "unknown"){
+                                console.log(new Date().toLocaleString() + " " + dev.name + " turned on!");
                                 updateNow = true;
+                                deviceList[dev.id].recentlyChanged = true;
                             }
 
                             if (prevUsage != -1 && Math.abs(usageDelta) > usageThreshold){
-                                console.log(dev.name + " usage changed by " + usageDelta);
-                                updateNow = true;
+                                console.log(new Date().toLocaleString() + " " + dev.name + " usage changed by " + usageDelta);
+                                usageUpdate = true;
+                                deviceList[dev.id].recentlyChanged = true;
                             }
                             deviceList[dev.id].state = "on";
                             deviceList[dev.id].usage = dev.w;
-                            deviceList[dev.id].refreshed = 1;
+                            deviceList[dev.id].currentlyOn = true;
                         }
                         
                         //Keep track of the total so we can send an overall total device
@@ -124,23 +133,33 @@ function getData(){
                         Object.keys(deviceList).forEach(function (key){
                             devArray.push(deviceList[key]);
                             senseTotal.usage = senseTotal.usage + deviceList[key].usage;
-                            if (deviceList[key].refreshed == 0){
+                            if (deviceList[key].currentlyOn == false){
                                 if (deviceList[key].state != "off" && deviceList[key].state != "unknown" && deviceList[key].name != "Other"){
-                                    console.log(deviceList[key].name + " turned off!");
+                                    console.log(new Date().toLocaleString() + " " + deviceList[key].name + " turned off!");
                                     updateNow = true;
+                                    deviceList[key].recentlyChanged = true;
                                 }
                                 deviceList[key].state = "off";
                                 deviceList[key].usage = 0;
                             }
                         })
 
+                        //Push total usage update if it's changed recently
+                        if (usageUpdate || (Math.abs(senseTotal.usage - prevTotalUsage) > totalUsageThreshold)){
+                            senseTotal.recentlyChanged = true
+                        }
+
                         //Add in "total" device
                         devArray.push(senseTotal);
+                        prevTotalUsage = senseTotal.usage;  //Save current total for future comparison
 
                         var secondsSinceLastPush = (Date.now() - lastPush.getTime()) / 1000
                         
+                        //If usage has changed and it's been awhile to avoid spamming, go ahead and send another update
+                        if (!updateNow && usageUpdate && secondsSinceLastPush >= minimumSecondsBetweenPush){updateNow = true;}
+
                         //Override updateNow if it's been less than 15 seconds since our last push
-                        if (secondsSinceLastPush <= minimumSecondsBetweenPush){updateNow = false;} 
+                        //if (secondsSinceLastPush <= minimumSecondsBetweenPush){updateNow = false;}
 
                         if (updateNow || secondsSinceLastPush >= maximumsecondsBetweenPush){
                             //console.log("Sending data to SmartThings hub");
@@ -158,7 +177,7 @@ function getData(){
                             //Send to SmartThings!
                             rp(options)
                                 .then(function (parsedBody) {
-                                        console.log(new Date().toLocaleString() + " - Data successfully sent to SmartThings hub!");
+                                        console.log(new Date().toLocaleString() + " **Data successfully sent to SmartThings hub!**");
                                 })
                                 .catch(function (err) {
                                     console.log("Error sending data to ST: " & err);           
---
+++ node_server/unofficial-sense.js
@@ -0,0 +1,104 @@
+const fetch = require('node-fetch'), ws = require('ws'), EventEmitter = require('events');
+
+
+const apiURL = 'https://api.sense.com/apiservice/api/v1/'
+
+var emmitter = new EventEmitter();
+
+var senseWS = null;
+
+var verbose = true;
+
+const setupWS = (onData) => {
+    const sendData = typeof onData == 'function'
+    let WSURL = `wss://clientrt.sense.com/monitors/${authData.monitors[0].id}/realtimefeed?access_token=${authData.access_token}`
+    senseWS = new ws(WSURL)
+
+    senseWS.on('open', () => {
+        if(sendData) {
+            onData({
+                status: "Connected"
+            })
+        }
+    });
+    senseWS.on('message', (data) => {
+        emmitter.emit('data', JSON.parse(data));
+        if(sendData) {
+            onData({
+                status: "Received",
+                data: JSON.parse(data)
+            })
+        } else {
+            if (verbose){console.log(data);}
+        }
+    });
+    senseWS.onclose = (data) => {
+        emmitter.emit('close', data);        
+        if (verbose){console.log("Connection closed: " + data);}
+     };
+    senseWS.onerror = (data) => {
+        emmitter.emit('error',data);
+        if (verbose){console.log("Error: " + data);}
+    };
+}
+var authData = {};
+module.exports = async (config, onData) => {
+    return new Promise( async (resolve, reject) => {
+        if(!config.email || !config.password) {
+            throw new Error('Config missing required parameters, needs email and password (optional base64)')
+        }
+        if(Buffer.from(config.password).toString('base64') === config.password) {
+            config.password = Buffer.from(config.password.toString('base64'))
+        }
+        if (config.verbose != undefined){verbose = config.verbose};
+
+        authData = await (await fetch(`${apiURL}authenticate`, { method: 'POST', body: `email=${config.email}&password=${config.password}`, headers: {"Content-Type":"application/x-www-form-urlencoded"} })).json()
+        setInterval(async () => {
+            authData = await (await fetch(`${apiURL}authenticate`, { method: 'POST', body: `email=${config.email}&password=${config.password}`, headers: {"Content-Type":"application/x-www-form-urlencoded"} })).json()
+            setupWS(onData)
+        }, 900000)
+        //console.log(authData);
+        if(authData.authorized) {
+            if(typeof onData == 'function') {
+                onData({
+                    status: 'Authenticated',
+                    data: authData.monitors
+                })
+            }
+            setupWS(onData);
+            resolve({
+                getAuth: async () => {
+                    authData = await (await fetch(`${apiURL}authenticate`, { method: 'POST', body: `email=${config.email}&password=${config.password}`, headers: {"Content-Type":"application/x-www-form-urlencoded"} })).json()   
+                    setupWS(onData)
+                    return authData
+                },
+                events: emmitter,
+                getDevices: async () => {
+                    return new Promise( async (resolve, reject) => {
+                        const devices = (await (await fetch(`${apiURL}app/monitors/${authData.monitors[0].id}/devices`, { method: 'GET', headers: {"Authorization": `bearer ${authData.access_token}`} })).json())
+                        resolve(devices)
+                    })
+                },
+                getMonitorInfo: async () => {
+                    return new Promise( async (resolve, reject) => {
+                        const monitor = (await (await fetch(`${apiURL}app/monitors/${authData.monitors[0].id}/status`, { method: 'GET', headers: {"Authorization": `bearer ${authData.access_token}`} })).json())
+                        resolve(monitor)
+                    })
+                },
+                getTimeline: async () => {
+                    return new Promise( async (resolve, reject) => {
+                        const user = (await (await fetch(`${apiURL}users/${authData.user_id}/timeline`, { method: 'GET', headers: {"Authorization": `bearer ${authData.access_token}`} })).json())
+                        resolve(user)
+                    })
+                }
+            })
+        } else if(authData.status == 'error') {
+            emmitter.emit('error', authData.error_reason);
+            reject(new Error(authData.error_reason));
+        } else {
+            emmitter.emit('error', 'Unable to make auth request');
+            reject(new Error('Unable to make auth request'));
+        }
+    });
+}
+
