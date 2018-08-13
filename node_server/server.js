fce213de0db22962d0ba5217e15f2bdd84ca67f6
Brian Beaird <brbeaird@gmail.com>
2018-08-13 10:17:59 -0500

    Added handling for when Sense discovers a new device Device will be created in ST the first time it's seen as "On"

--- node_server/server.js
+++ node_server/server.js
@@ -8,10 +8,10 @@ var smartThingsHubIP = config.smartThingsHubIP;
 //Optional Settings
 /*This process opens up a websocket connection to Sense; we see realtime data from Sense every couple seconds.
 While parsing that data, we watch for significant changes so we can push to SmartThings only when relevant to avoid spamming the hub.
-At baseline, we send an update at least once every 3 minutes (maximumsecondsBetweenPush).
-We do NOT send updates less than 5 seconds apart (minimumSecondsBetweenPush).
+At baseline, we send an update at least once every minute (maximumsecondsBetweenPush).
+We do NOT send updates less than 10 seconds apart (minimumSecondsBetweenPush).
 We send an update when at least one device has turned on or off.
-We send an update when power usage on a device has changed by at least 100 watts (usageThreshold).
+We send an update when power usage on a device has changed by at least 200 watts (usageThreshold).
 */
 var usageThreshold = 200;    //Change in usage that determines when a special push to ST is made
 var maximumsecondsBetweenPush = 60; //Maximum number of seconds between data pushes to ST
@@ -29,6 +29,22 @@ lastPush.setDate(lastPush.getDate()-1);
 global.reconnectPending = false;
 global.prevTotalUsage = 0;
 
+
+function addDevice(dev){
+    let devName = dev.name;
+    if (dev.tags.NameUserGuess == "true"){ 
+        devName = devName + " (?)" 
+    }
+    deviceList[dev.id] = {
+        id: dev.id,
+        name: devName,
+        state: "unknown",
+        usage: -1,
+        currentlyOn: false,
+        recentlyChanged: true
+    }
+}
+
 getData();
 
 function getData(){
@@ -43,19 +59,8 @@ function getData(){
         reconnectPending = false;
         mySense.getDevices().then(devices => {
             for (let dev of devices){                
-                let devName = dev.name;
-                if (dev.tags.NameUserGuess == "true"){ 
-                    devName = devName + " (?)" 
-                }
-                deviceList[dev.id] = {
-                    id: dev.id,
-                    name: devName,
-                    state: "unknown",
-                    usage: -1,
-                    currentlyOn: false,
-                    recentlyChanged: true
-                }
-            }        
+                addDevice(dev);
+            }
         })
 
         //Handle websocket data updates
@@ -85,101 +90,105 @@ function getData(){
         //Process the data from Sense!
         function processData(data){
             if (data.type == "realtime_update"){            
-                if (data.payload != undefined){
-                    if (data.payload.devices != undefined){                        
-                        let updateNow = false;                        
+                if (data.payload && data.payload.devices){                    
+                    let updateNow = false;                        
 
-                        //Mark off saved list so we can detect which have been seen lately
-                        Object.keys(deviceList).forEach(function (key){                            
-                            deviceList[key].currentlyOn = false;                            
-                            if (deviceList[key].usage != -1){deviceList[key].recentlyChanged = false;}  //Reset recentChange flag unless it's initial load                            
-                        })
+                    //Mark off saved list so we can detect which have been seen lately
+                    Object.keys(deviceList).forEach(function (key){                            
+                        deviceList[key].currentlyOn = false;                            
+                        if (deviceList[key].usage != -1){deviceList[key].recentlyChanged = false;}  //Reset recentChange flag unless it's initial load                            
+                    })
 
-                        //Loop over currently active devices and refresh the saved list
-                        // We'll send data to SmartThings if status has changed or if usage has changed over a certain amount
-                        for (let dev of data.payload.devices){
-                            let prevState = deviceList[dev.id].state;
-                            let prevUsage = deviceList[dev.id].usage;
-                            let currentUsage = dev.w;
-                            let usageDelta = currentUsage - prevUsage;
-                            
-                            //Don't go below 1 watt
-                            if (deviceList[dev.id].usage < 1){deviceList[dev.id].usage = 1}
-                            
-                            if (prevState != "on" && prevState != "unknown" && dev.name != "Other"){
-                                console.log(new Date().toLocaleString() + " " + dev.name + " turned on!");
-                                updateNow = true;
-                                deviceList[dev.id].recentlyChanged = true;
-                            }
-
-                            if (prevUsage != -1 && Math.abs(usageDelta) > usageThreshold && dev.name != "Other"){
-                                console.log(new Date().toLocaleString() + " " + dev.name + " usage changed by " + usageDelta);
-                                updateNow = true;
-                            }
-                            deviceList[dev.id].state = "on";
-                            deviceList[dev.id].usage = dev.w;
-                            deviceList[dev.id].currentlyOn = true;
-                        }
+                    //Loop over currently active devices and refresh the saved list
+                    // We'll send data to SmartThings if status has changed or if usage has changed over a certain amount
+                    for (let dev of data.payload.devices){
                         
-                        //Keep track of the total so we can send an overall total device
-                        let senseTotal = {
-                            id: "TotalUsage",
-                            name: "TotalUsage",
-                            state: "on",
-                            usage: 0
+                        //If this is a new device, make a new spot for it
+                        if (!deviceList[dev.id]){
+                            addDevice(dev);
                         }
 
-                        //Convert list to array for easier parsing in ST
-                        var devArray = [];
-
-                        //Loop over saved list again and mark any remaining devices as off
-                        Object.keys(deviceList).forEach(function (key){                            
-                            senseTotal.usage = senseTotal.usage + deviceList[key].usage;
-                            if (deviceList[key].currentlyOn == false){
-                                if (deviceList[key].state != "off" && deviceList[key].state != "unknown" && deviceList[key].name != "Other"){
-                                    console.log(new Date().toLocaleString() + " " + deviceList[key].name + " turned off!");
-                                    updateNow = true;
-                                    statusChange = true;
-                                    deviceList[key].recentlyChanged = true;
-                                }
-                                deviceList[key].state = "off";
-                                deviceList[key].usage = 0;
-                            }
-                            devArray.push(deviceList[key]);
-                        })
-
-                        //Add in "total" device
-                        devArray.push(senseTotal);                        
-
-                        var secondsSinceLastPush = (Date.now() - lastPush.getTime()) / 1000                        
-
-                        //Override updateNow if it's been less than 15 seconds since our last push
-                        if (secondsSinceLastPush <= minimumSecondsBetweenPush){updateNow = false;}                        
-
-                        if (updateNow || secondsSinceLastPush >= maximumsecondsBetweenPush){
-                            //console.log("Sending data to SmartThings hub");
-                            prevTotalUsage = senseTotal.usage;  //Save current total for future comparison
-                            lastPush = new Date();
-                            var options = {
-                                method: 'POST',
-                                uri: 'http://' + smartThingsHubIP + ':39500/event',
-                                body: {"devices": devArray},
-                                headers: {
-                                    'source': 'STSense'
-                                },
-                                json: true // Automatically stringifies  the body to JSON
-                            };
-                            
-                            //Send to SmartThings!
-                            rp(options)
-                                .then(function (parsedBody) {
-                                        console.log(new Date().toLocaleString() + " **Data successfully sent to SmartThings hub!**");
-                                })
-                                .catch(function (err) {
-                                    console.log("ERROR: Unable to connect to SmartThings Hub: " + err.message);                                    
-                                });
+                        let prevState = deviceList[dev.id].state;
+                        let prevUsage = deviceList[dev.id].usage;
+                        let currentUsage = dev.w;
+                        let usageDelta = currentUsage - prevUsage;
+                        
+                        //Don't go below 1 watt
+                        if (deviceList[dev.id].usage < 1){deviceList[dev.id].usage = 1}
+                        
+                        if (prevState != "on" && prevState != "unknown" && dev.name != "Other"){
+                            console.log(new Date().toLocaleString() + " " + dev.name + " turned on!");
+                            updateNow = true;
+                            deviceList[dev.id].recentlyChanged = true;
                         }
+
+                        if (prevUsage != -1 && Math.abs(usageDelta) > usageThreshold && dev.name != "Other"){
+                            console.log(new Date().toLocaleString() + " " + dev.name + " usage changed by " + usageDelta);
+                            updateNow = true;
+                        }
+                        deviceList[dev.id].state = "on";
+                        deviceList[dev.id].usage = dev.w;
+                        deviceList[dev.id].currentlyOn = true;
                     }
+                    
+                    //Keep track of the total so we can send an overall total device
+                    let senseTotal = {
+                        id: "TotalUsage",
+                        name: "TotalUsage",
+                        state: "on",
+                        usage: 0
+                    }
+
+                    //Convert list to array for easier parsing in ST
+                    var devArray = [];
+
+                    //Loop over saved list again and mark any remaining devices as off
+                    Object.keys(deviceList).forEach(function (key){                            
+                        senseTotal.usage = senseTotal.usage + deviceList[key].usage;
+                        if (deviceList[key].currentlyOn == false){
+                            if (deviceList[key].state != "off" && deviceList[key].state != "unknown" && deviceList[key].name != "Other"){
+                                console.log(new Date().toLocaleString() + " " + deviceList[key].name + " turned off!");
+                                updateNow = true;
+                                statusChange = true;
+                                deviceList[key].recentlyChanged = true;
+                            }
+                            deviceList[key].state = "off";
+                            deviceList[key].usage = 0;
+                        }
+                        devArray.push(deviceList[key]);
+                    })
+
+                    //Add in "total" device
+                    devArray.push(senseTotal);                        
+
+                    var secondsSinceLastPush = (Date.now() - lastPush.getTime()) / 1000                        
+
+                    //Override updateNow if it's been less than 15 seconds since our last push
+                    if (secondsSinceLastPush <= minimumSecondsBetweenPush){updateNow = false;}                        
+
+                    if (updateNow || secondsSinceLastPush >= maximumsecondsBetweenPush){
+                        //console.log("Sending data to SmartThings hub");
+                        prevTotalUsage = senseTotal.usage;  //Save current total for future comparison
+                        lastPush = new Date();
+                        var options = {
+                            method: 'POST',
+                            uri: 'http://' + smartThingsHubIP + ':39500/event',
+                            body: {"devices": devArray},
+                            headers: {
+                                'source': 'STSense'
+                            },
+                            json: true // Automatically stringifies  the body to JSON
+                        };
+                        
+                        //Send to SmartThings!
+                        rp(options)
+                            .then(function (parsedBody) {
+                                    console.log(new Date().toLocaleString() + " **Data successfully sent to SmartThings hub!**");
+                            })
+                            .catch(function (err) {
+                                console.log("ERROR: Unable to connect to SmartThings Hub: " + err.message);                                    
+                            });
+                    }                    
                 }
             }
         }
